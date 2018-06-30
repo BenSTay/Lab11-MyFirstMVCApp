64dcf16a081b0174d4cd1cef91e0ce46081f1e58
Benjamin Taylor <staylor.ben@gmail.com>
2018-06-29 20:21:32 -0700

    finished lab

---
+++ PersonOfTheYear/PersonOfTheYear/Models/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace PersonOfTheYear.Models
+{
+    public class Person
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        [Display(Name = "Birth Year")]
+        public int Birth_Year { get; set; }
+
+        [Display(Name = "Death Year")]
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
+        /// <summary>
+        /// Creates a list of people named 'person of the year' by TIME between the two given dates
+        /// </summary>
+        /// <param name="start">The lower limit of the search.</param>
+        /// <param name="end">The upper limit of the search.</param>
+        /// <returns>A list of people.</returns>
+        public static List<Person> GetPeople(int start, int end)
+        {
+            List<Person> people = new List<Person>();
+
+            string path = Path.GetFullPath(Path.Combine(
+                Environment.CurrentDirectory, @"Data\personOfTheYear.csv"));
+
+            foreach (string line in File.ReadAllLines(path))
+            {
+                string[] fields = line.Split(',');
+                if (fields[0] == "Year") continue;
+
+                people.Add(new Person
+                {
+                    Year = Convert.ToInt32(fields[0]),
+                    Honor = fields[1],
+                    Name = fields[2],
+                    Country = fields[3],
+                    Birth_Year = (fields[4] == "") ? 0 : Convert.ToInt32(fields[4]),
+                    DeathYear = (fields[5] == "") ? 0 : Convert.ToInt32(fields[5]),
+                    Title = fields[6],
+                    Category = fields[7],
+                    Context = fields[8]
+                });
+            }
+
+            return people.Where(p => (p.Year >= start) && (p.Year <= end)).ToList();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Lab11-MyFirstMVCApp
+**Author**: Benjamin Taylor  
+**Version**: 1.0.0
+
+## Overview
+A web application that displays information about TIME "Person of the Year" recipients between two years entered by the user.
+
+## Getting Started
+1. Create a fork of this repository, and clone your fork to your device.  
+2. Open the solution file (PersonOfTheYear.sln) in Visual Studio.
+3. To run the app, click the green arrow button labeled "IIS Express".
+
+## Using The Application
+![screenshot](https://github.com/btaylor93/Lab11-MyFirstMVCApp/raw/master/assets/screenshot.jpg)
+1. Upon starting the application, the home page will be displayed in a new browser window, as seen in the above screenshot.
+2. On the home page, you should see a form with two number entry fields. Enter a year in between 1927 and 2016 in each field and then press the submit button to begin a search.
+3. After hitting the submit button, the browser should be re-directed to a page containing a table that holds entries for each TIME "Person of the Year" recipient between the selected dates.
+
+## Architecture
+**Languages Used**:
+* C# 7.0 (.NET Core 2.1)
+* HTML
+* CSS
+
+Written with Visual Studio Community 2017.
+
+## Change Log
+06-29-2018 8:04pm - Initial version.
